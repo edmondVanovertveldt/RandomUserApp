8e8a5fb5969c1a606e45510ca54622d3e0b0e68b
Edmond Van Overtveldt <edmond.vanovertveld@alphacredit.be>
2025-06-01 15:06:38 +0200

    Initial Commit

---
+++ RandomUserTests/RandomUserTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserTests.swift
+//  RandomUserTests
+//
+//  Created by edmond vanovertveldt on 01/06/2025.
+//
+
+import Testing
+@testable import RandomUser
+
+struct RandomUserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
